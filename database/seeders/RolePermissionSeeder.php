a82ac92e56ad70e47edd6ac90032544163720190
Abid Hasan <abidtkg@gmail.com>
2024-12-07 23:52:01 +0600

    initial

---
+++ database/seeders/RolePermissionSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
+class RolePermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $role = Role::create(['name' => 'admin']);
+
+        $permissions = [
+            ['name' => 'user list'],
+            ['name' => 'create user'],
+            ['name' => 'edit user'],
+            ['name' => 'delete user']
+        ];
+
+        foreach($permissions as $permission){
+            Permission::create($permission);
+        }
+        $user = User::first();
+        // $user->assignRole($role);
+    }
+}
